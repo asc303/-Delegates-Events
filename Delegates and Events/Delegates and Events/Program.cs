45903c274a8984b826f0fa167ea14ac45aa98f1f
asc303 <chauhansam94@gmail.com>
2021-11-26 15:14:10 +0530

    [ashutosh] add:Program for Singlecast Delegate

--- Delegates and Events/Delegates and Events/Program.cs
+++ Delegates and Events/Delegates and Events/Program.cs
@@ -7,9 +7,7 @@ namespace Delegates_and_Events
         static void Main(string[] args)
         {
             operation obj = new operation(SinglecastDelegate.Addition); // Object Creating  For Single Delegate
-            Console.WriteLine("Addition is: {0}", obj(25, 25));
-            MulticastDelegate.ImplementDelegate(); // Directly Call of a Function Using Class
-            Events.ImplementEvent(); 
+            Console.WriteLine("Addition is: {0}", obj(25, 25));            
         }
     }
 }
